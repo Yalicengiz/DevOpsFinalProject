5db8d458def23c9cf5d7989e8df490de5b4c838f
Yalicengiz <yunusalicengiz@gmail.com>
2024-03-08 16:42:09 +0300

    v3

---
+++ Yeni klasör/src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>Merhaba, Dünya!</h1>
+      <p>Bu, DevOps Projesidir.</p>
+    </div>
+  );
+}
+
+export default MyComponent;
